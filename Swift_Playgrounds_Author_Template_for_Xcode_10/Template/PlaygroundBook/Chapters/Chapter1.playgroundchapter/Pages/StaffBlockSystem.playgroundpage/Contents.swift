41b7e7b305c7df12cf155fd29fa7d72dc0094c25
Qingyang Hu <huqingyang2000@gmail.com>
2019-03-24 10:53:28 +0800

    ...

--- Swift_Playgrounds_Author_Template_for_Xcode_10/Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/StaffBlockSystem.playgroundpage/Contents.swift
+++ Swift_Playgrounds_Author_Template_for_Xcode_10/Template/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/StaffBlockSystem.playgroundpage/Contents.swift
@@ -6,7 +6,7 @@ typealias Train = TrainNode
 //#-end-hidden-code
 /*:
  # Staff Block System
- One of the traditional ways is called [Staff block system](glossary://staff%20block%20system). In railway, a track is divided into small regions called '[blocks](glossary://Signalling%20block%20system)'. 'Staff block system' is a type of the [manual block system](glossary://Manual%20block%20system). A symbol, which is often a ticket or a staff, is required for the train to run in a specific region. This is often used for single track railways, where only one train is allowed to run in a region.
+ One of the traditional ways is called [staff block system](glossary://staff%20block%20system). In railway, a track is divided into small regions called '[blocks](glossary://signalling%20block%20system)'. 'Staff block system' is a type of the [manual block system](glossary://manual%20block%20system). A symbol, which is often a ticket or a staff, is required for the train to run in a specific region. This is often used for single track railways, where only one train is allowed to run in a region.
  
  Now you can control the maximum speed of each train. Try changing the parameters and see how it would affect efficiency. T
  
